90b1ec2335ee5e3d083e6827f94e006b5466f6f5
Koronashi <109332110+Koronashi@users.noreply.github.com>
2024-06-16 18:20:10 +0800

    update Main.js

--- Main.js
+++ Main.js
@@ -1,9 +1,10 @@
 const favQuotes = [
-  "We cannot solve our problems with the same thinking we used when we created them."
+  "We cannot solve our problems with the same thinking we used when we created them.",
+  "You can't blame gravity for falling in love.",
+  "Once we accept our limits, we go beyond them.",
+  "Intellectuals solve problems, geniuses prevent them.",
+  "Kids who have never seen peace and kids who have never seen war have different values! Those who stand at the top determine what's wrong and what's right! This very place is neutral ground! Justice will prevail, you say?"
 ];
-const author = [
-  "Albert Einstein"
-]
 
 function getRandomString() {
   const randomIndex = Math.floor(Math.random() * favQuotes.length);
@@ -11,10 +12,12 @@ function getRandomString() {
 }
 
 function getAuthor(x) {
-  if (x = 1) {
+  if (x <= 4) {
     return "Albert Einstein";
+  } else if (x <= 5) {
+    return "Donquixote Doflamingo";
   }
 }
 
 document.getElementById("qu").innerHTML = favQuotes[getRandomString()];
-document.getElementById("aut").innerHTML = "-" + getAuthor(getRandomString());
+document.getElementById("aut").innerHTML = "- " + getAuthor(getRandomString());
